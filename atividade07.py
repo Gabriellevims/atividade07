cc142deeca8347611c55f2a1c50bb93c22e9fa6a
gabriellevims <gabriellevi2104@gmail.com>
2023-10-18 11:14:03 -0300

    adicionar atividade07

---
+++ atividade07.py
@@ -0,0 +1,4 @@
+# Digite um valor e veja quantos dolares voce poderá comprar: R$
+valor = float(input("digite o valor R$:"))
+dolares = valor/5
+print("seu dinheiro em reais em dolares é: %.2f " % dolares )
